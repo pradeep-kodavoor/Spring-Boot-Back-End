2eff07ed51acf6902426e2e1a8aedef4c5e14f4e
734456 <734456@PC363420.cts.com>
2018-11-29 12:04:23 +0530

    Initial Project Setup

---
+++ src/main/java/com/practice/rest/webservices/SpringBootBackEnd/SpringBootBackEndApplication.java
@@ -0,0 +1,12 @@
+package com.practice.rest.webservices.SpringBootBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackEndApplication.class, args);
+	}
+}
---
+++ src/test/java/com/practice/rest/webservices/SpringBootBackEnd/SpringBootBackEndApplicationTests.java
@@ -0,0 +1,16 @@
+package com.practice.rest.webservices.SpringBootBackEnd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootBackEndApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
